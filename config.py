d35e3d7fd528d8ba0319ee19931e43bc2ff80a06
Sergey <goryachihs@yandex.ru>
2017-11-16 14:41:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = '396203698:AAE6HEHpu-2w1jD5BWxWQPiaCHLNOycWp2M' #Токен бота
+token = '' #Токен бота
 directory_music = '/home/telegram/music/' #Директория,в который будут лежать файлы с музыкой
 directory_foto = '/home/telegram/photos/' #Директория,в которой будут лежать фотографии
 directory_document = '/home/telegram/documents/' #Директория,в которой будут лежать документы
